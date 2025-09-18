6d5e334d4a06635b7fc642670058d7dbc1200ef6
igormikt <tigormik@gmail.com>
2025-09-18 14:20:47 +0300

    cor app

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,24 @@
 from flask import render_template, request, redirect, url_for
-
 from app import app
-posts = []
-@app.route("/", methods=["GET", "POST"])
 
+users = []
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-#использует метод POST, так как информация будет отправляться. Request method сравнивает данные с HTTP-запросом.
-    if request.method == 'POST':
-    #функция request.form извлекает значение из соответствующих полей
-        title = request.form.get('title')
-        content = request.form.get('content')
-        #создаёт условие для проверки наличия данных в полях title и content
-        if title and content:
-           posts.append({'title': title, 'content': content})
-        #использует для обновления страницы и предотвращения повторной отправки формы.
-        return redirect(url_for('index'))
-        #возвращает отрендеренный шаблон с переданными данными постов
-    return render_template('blog.html', posts=posts)
+    if request.method == "POST":
+        name = request.form.get('name')
+        age = request.form.get('age')
+        city = request.form.get('city')
+        hobby = request.form.get('hobby')
+
+        # Проверка на заполнение обязательных полей
+        if name and age and city and hobby:
+            user_data = {
+                'name': name,
+                'age': age,
+                'city': city,
+                'hobby': hobby
+            }
+            users.append(user_data)
+
+    return render_template('form.html', users=users)
